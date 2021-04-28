26b0b3a44d39a9e7389a153f41606cbb244999e6
Stein Bergervoet <stein@steins-mbp.home>
2021-04-28 14:48:11 +0200

    Translated the designs into front-end code and changed the content of the README.md file.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# project-tech
-Mijn uitgewerkte feature voor Project Tech.
+# DogeMeet
+
+Welkom! Dit is mijn repo voor mijn school project voor Project Tech. Ik heb tijdens dit project gewerkt aan een like- en zoekfunctie van mijn matchingsplatform DogeMeet.
+
+# Het Concept
+
+Met behulp van mijn platform DogeMeet kunnen mensen met elkaar worden verbonden gebaseerd op hun huisdieren (voornamelijk honden, maar alles wat aan een riem kan lopen mag). Gebruikers kunnen op een soortgelijke manier als Tinder door de huisdieren van andere gebruikers heen “swipen”. Wanneer twee mensen elkaar een like geven kunnen ze met elkaar chatten. Huisdiereigenaren kunnen nu dus bijvoorbeeld afspreken om met elkaar een wandeling te maken. Zo komen zij en hun huisdieren in contact met anderen.
+
+# Installeren 
+
+Je kan mijn project installeren door mijn repo lokaal te klonen. Vervolgens moet je via de terminal (in de lokale folder) de command `npm install` uitvoeren. Hiervoor moet natuurlijk NPM en Node wel geïnstalleerd zijn op je machine. Door het commando `npm run start-server` uit te voeren kan mijn feature bekeken worden op localhost met port 4200. 
+
+> **NOTE:** Mijn feature is gemaakt voor mobiele formaten en zal er op desktop computers niet al te best uit zien.  
+
+# De technologieën 
+
+Voor het uitwerken van mijn feature heb ik een aantal technologieën gebruikt: 
+* Node.js
+* Express
+* Pug
+
+# License
+
+MIT License 
--- server.js
+++ server.js
@@ -4,18 +4,49 @@ const port = 4200;
 
 pageHeaders = ['DogeMeet home', 'DogeMeet profile', 'DogeMeet search'];
 
+dogeExample = {
+    name: 'Kenji',
+    age: 6, 
+    breed: 'Shiba Inu',
+    image_link: '/public/images/doge_example_kenji.png',
+    doge_vibe: ['relaxed', 'cute', 'fluffy'],
+    description: 'Heyo, dit is mijn Shiba Inu Kenji. Hij houd erg van andere honden en buiten spelen!',
+    description_long: 'Heyo, dit is mijn Shiba Inu Kenji. Hij houd erg van andere honden en buiten spelen! Kenji is heel goed met kinderen en zijn favoriete eten is chips, maar dat krijgt hij niet te vaak hoor!'
+}
+
+locationsExample = [{
+    name: 'Het Amsterdamse Bos',
+    location_distance: '400m'
+},
+{
+    name: 'Een ander bos',
+    location_distance: '1.2km'
+},
+{
+    name: 'Nog een bos',
+    location_distance: '1.5km'
+},
+{ 
+    name: 'Een super cool strand',
+    location_distance: '2.4km'
+},
+{ 
+    name: 'Park Pannenkoek',
+    location_distance: '4km'
+}];
+
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => { 
-    res.render('home', { pageHeader: pageHeaders[0], user: { name: 'Henk' }});
+    res.render('home', { doge: dogeExample, location: locationsExample[0] });
 });
 
 app.get('/profile', (req, res) => { 
-    res.render('profile', { pageHeader: pageHeaders[1]});
+    res.render('profile', { doge: dogeExample, location: locationsExample[0] });
 });
 
 app.get('/search', (req, res) => { 
-    res.render('search', { pageHeader: pageHeaders[2]});
+    res.render('search', { locations: locationsExample});
 });
 
 app.use(express.static('static'));
