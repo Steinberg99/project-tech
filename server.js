d58bd549d1e0f03368d36b0839b013bf67b772c4
System Administrator <root@steins-mbp.home>
2021-05-26 18:53:51 +0200

    Added the backend of the feature

--- server.js
+++ server.js
@@ -1,209 +1,196 @@
+const { parse } = require('dotenv');
+const { query } = require('express');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const app = express();
+let database = null;
 let port = process.env.PORT;
 
-const client = new MongoClient(process.env.DB_URI, {
-  retryWrites: true,
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-});
-
-//The current example doggos. These will eventualy be moved to the database.
-const doggos = [
-  {
-    id: 1,
-    name: 'Kenji',
-    age: 6,
-    breed: 'Shiba Inu',
-    image_link: '/public/images/doge_example_kenji.png',
-    profile_image_link: '/public/images/doge_example_kenji_profile.png',
-    doge_vibe: ['relaxed', 'cute', 'fluffy'],
-    description:
-      'Heyo, dit is mijn Shiba Inu Kenji. Hij houd erg van andere honden en buiten spelen!'
-  },
-  {
-    id: 2,
-    name: 'Scooby',
-    age: 10,
-    breed: 'Mix',
-    image_link: '/public/images/doge_example_scooby.png',
-    profile_image_link: '/public/images/doge_example_scooby_profile.png',
-    doge_vibe: ['relaxed', 'cute'],
-    description:
-      'Hallo! Dit is mijn hond Scooby. Hij is een beetje ondeugend maar wel heel lief.'
-  },
-  {
-    id: 3,
-    name: 'Fred',
-    age: 3,
-    breed: 'Pug',
-    image_link: '/public/images/doge_example_fred.png',
-    profile_image_link: '/public/images/doge_example_fred_profile.png',
-    doge_vibe: ['cute', 'chubby'],
-    description:
-      'Fred is een beetje dik maar wel heel cute. Kijk z’n tong uitsteken dan!'
-  }
-];
-
-//The currect example locations. These would also eventualy be moved to the database.
-const locations = [
-  {
-    name: 'Het Amsterdamse Bos',
-    distance: '400m',
-    type: 'forest'
-  },
-  {
-    name: 'Super cool meer',
-    distance: '700m',
-    type: 'lake'
-  },
-  {
-    name: 'Een mooie straat',
-    distance: '900m',
-    type: 'city_walk'
-  },
-  {
-    name: 'Een ander bos',
-    distance: '1.2km',
-    type: 'forest'
-  },
-  {
-    name: 'Wow, nog een bos',
-    distance: '1.5km',
-    type: 'forest'
-  },
-  {
-    name: 'Deens park',
-    distance: '1.7km',
-    type: 'park'
-  },
-  {
-    name: 'Een super cool strand',
-    distance: '2.4km',
-    type: 'beach'
-  },
-  {
-    name: 'Park Pannenkoek',
-    distance: '4km',
-    type: 'park'
-  },
-  {
-    name: 'Een strand',
-    distance: '4.6km',
-    type: 'beach'
-  },
-  {
-    name: 'Bazinga meer',
-    distance: '6.3km',
-    type: 'lake'
-  }
-];
-
-//Stores the liked and disliked doggos. This would eventualy be moved to the database.
-let likedDoggos = [];
-let dislikedDoggos = [];
-//Stores the current doggo displayed on the screen.
-let currentDoggo;
-
-//Test function for connecting with the database.
-async function connectDB() {
-  console.log(client);
-  try {
-    await client.connect();
-    await listDatabases(client);
-  } catch (e) {
-    console.log(e);
-  } finally {
-    await client.close();
-  }
-}
-
-connectDB().catch(console.error);
-
-//Test function that lists all the connceted databases.
-async function listDatabases(client) {
-  const databasesList = await client.db().admin().listDatabases();
-  databasesList.databases.forEach(db => {
-    console.log(db.name);
-  });
-}
-
-//Function that returns a doge profile based on a gived profileId.
-function getDogeProfile(id) {
-  return doggos[id - 1];
-}
-
-//Function that finds the next doggo that has not yet been liked or disliked.
-function nextNotLikedOrDislikedDoge() {
-  for (let i = 0; i < doggos.length; i++) {
-    if (
-      !likedDoggos.includes(doggos[i]) &&
-      !dislikedDoggos.includes(doggos[i])
-    ) {
-      return doggos[i];
-    }
-  }
-  return null;
-}
+let doggos = null;
+let locations = null;
+let currentDoggo = null;
 
 app.set('view engine', 'pug');
 
 app.use(express.urlencoded({ extended: true }));
 
-//Reders the homepage.
-app.get('/', (req, res) => {
-  currentDoggo = nextNotLikedOrDislikedDoge();
+//Render the homepage.
+app.get('/', async (req, res) => {
+  //Retrieve the user data.
+  let user = await database
+    .collection('users')
+    .findOne({ name: 'Stein Bergervoet' });
+
+  //Retrieve the doggos array based on the last query the user submitted.
+  let query = {};
+  if (Object.keys(user.last_query).length !== 0) {
+    console.log(user.last_query);
+    query = createQuery(
+      user.last_query.age,
+      user.last_query.doge_vibes,
+      user.last_query.locations
+    );
+  }
+  doggos = await database.collection('doggos').find(query, {}).toArray();
+  doggos = await removeLikedOrDislikedDoggos();
+
+  currentDoggo = doggos[0];
+  let location = await getLocation(currentDoggo.location_id);
   res.render('home', {
-    doge: currentDoggo,
-    location: locations[0]
+    doggo: currentDoggo,
+    location: location
   });
 });
 
-//Reders the profile page based on the profileId in the url.
-app.get('/profile/:profileId', (req, res) => {
-  res.render('profile', {
-    doge: getDogeProfile(req.params.profileId),
-    location: locations[0]
-  });
-});
-
-//Reder the search page.
-app.get('/search', (req, res) => {
-  //Reset the liked and disliked arrays. Otherwise you cannot repeat the feature without restarting the server.
-  likedDoggos = [];
-  dislikedDoggos = [];
+//Render the search page.
+app.get('/search', async (req, res) => {
+  locations = await database.collection('locations').find({}, {}).toArray();
   res.render('search', { locations: locations });
 });
 
+//Render the homepage after the search form has been submitted.
+app.post('/search-result', async (req, res) => {
+  //Retrieve the doggos array based on the created query.
+  let query = createQuery(
+    req.body.age,
+    req.body.doge_vibes,
+    req.body.locations
+  );
+  doggos = await database.collection('doggos').find(query, {}).toArray();
+  doggos = await removeLikedOrDislikedDoggos();
+
+  //Store the query parameters for later use.
+  await database
+    .collection('users')
+    .updateOne(
+      { name: 'Stein Bergervoet' },
+      { $set: { last_query: req.body } }
+    );
+
+  //Render the homepage based on the content of the doggos array.
+  if (doggos[0]) {
+    currentDoggo = doggos[0];
+    let location = await getLocation(currentDoggo.location_id);
+    res.render('home', {
+      doggo: currentDoggo,
+      location: location
+    });
+  } else {
+    res.render('home', {
+      doggo: undefined,
+      location: undefined
+    });
+  }
+});
+
+function createQuery(age, dogeVibes, selectedLocations) {
+  let selectedLocationIds = selectedLocations.map(location =>
+    parseInt(location, 10)
+  );
+  return {
+    age: { $gt: 0, $lt: parseInt(age, 10) },
+    doge_vibe: { $in: dogeVibes },
+    location_id: { $in: selectedLocationIds }
+  };
+}
+
+async function removeLikedOrDislikedDoggos() {
+  let user = await database
+    .collection('users')
+    .findOne({ name: 'Stein Bergervoet' });
+
+  let filteredDoggos = [];
+  for (let i = 0; i < doggos.length; i++) {
+    if (
+      !user.liked_doggos.includes(doggos[i].id) &&
+      !user.disliked_doggos.includes(doggos[i].id)
+    ) {
+      filteredDoggos.push(doggos[i]);
+    }
+  }
+  console.log(filteredDoggos);
+  return filteredDoggos;
+}
+
+//Render the profile page.
+app.get('/profile/:profileId', async (req, res) => {
+  let doggoProfile = await getDoggoProfile(parseInt(req.params.profileId), 10);
+  let location = await getLocation(doggoProfile.location_id);
+  res.render('profile', {
+    doge: doggoProfile,
+    location: location
+  });
+});
+
+async function getDoggoProfile(id) {
+  return (profile = await database.collection('doggos').findOne({ id: id }));
+}
+
+async function getLocation(id) {
+  return (location = await database
+    .collection('locations')
+    .findOne({ id: id }));
+}
+
 //Render the liked doggos page.
-app.get('/liked', (req, res) => {
+app.get('/liked', async (req, res) => {
+  let likedDoggos = await getLikedDoggos();
   res.render('liked', { likedDoges: likedDoggos });
 });
 
-//Render the home page after searching for some doggos. This would eventualy be based on the search criteria.
-app.post('/search-result', (req, res) => {
-  currentDoggo = nextNotLikedOrDislikedDoge();
-  res.render('home', {
-    doge: currentDoggo,
-    location: locations[0]
-  });
+async function getLikedDoggos() {
+  let user = await database
+    .collection('users')
+    .findOne({ name: 'Stein Bergervoet' });
+
+  let likedDoggos = await database
+    .collection('doggos')
+    .find({ id: { $in: user.liked_doggos } }, {})
+    .toArray();
+
+  return likedDoggos;
+}
+
+//Render the home page after the user had liked or disliked a doggo.
+app.post('/like', async (req, res) => {
+  if (req.body.cute || req.body.cool || req.body.pretty) {
+    database
+      .collection('users')
+      .updateOne(
+        { name: 'Stein Bergervoet' },
+        { $push: { liked_doggos: currentDoggo.id } }
+      );
+  } else {
+    database
+      .collection('users')
+      .updateOne(
+        { name: 'Stein Bergervoet' },
+        { $push: { disliked_doggos: currentDoggo.id } }
+      );
+  }
+
+  currentDoggo = getNextDoggo();
+  if (currentDoggo != undefined) {
+    let location = await getLocation(currentDoggo.location_id);
+    res.render('home', {
+      doggo: currentDoggo,
+      location: location
+    });
+  } else {
+    res.render('home', {
+      doggo: undefined,
+      location: undefined
+    });
+  }
 });
 
-//Add the current doggo to the like or dislike array based on the user input. Render the next doggo that has not been liked or disliked.
-app.post('/like', (req, res) => {
-  if (req.body.cute || req.body.cool || req.body.pretty) {
-    likedDoggos.push(currentDoggo);
-  } else {
-    dislikedDoggos.push(currentDoggo);
+function getNextDoggo() {
+  for (let i = 0; i < doggos.length - 1; i++) {
+    if (currentDoggo == doggos[i]) return doggos[i + 1];
   }
-  currentDoggo = nextNotLikedOrDislikedDoge();
-  res.render('home', {
-    doge: currentDoggo,
-    location: locations[0]
-  });
-});
+  return undefined;
+}
 
 app.use(express.static('static'));
 
@@ -212,10 +199,28 @@ app.use((req, res) => {
   res.status(404).send('Error 404');
 });
 
+//Correct the value of the port when the app is running locally.
 if (port == null || port == '') {
   port = 4200;
 }
 
+async function connectDB() {
+  const client = new MongoClient(process.env.DB_URI, {
+    retryWrites: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  try {
+    await client.connect();
+    database = client.db(process.env.DB_NAME);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 app.listen(port, () => {
   console.log(`Listening at https://localhost:${port}`);
+  connectDB().then(() => {
+    console.log('Connected to MongoDB');
+  });
 });
